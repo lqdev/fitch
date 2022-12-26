5789e02a55bf0925eaec470b9daab5e944613fa0
lqdev <lqdev@outlook.com>
2022-12-25 19:26:02 -0500

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,56 @@
+﻿open Spectre.Console
+open Spectre.Console.Rendering
+open Utils
+
+let DisplayInfo () =
+    let distroId = 
+        getDistroId("/etc/os-release")
+
+    let distroName = 
+        getDistroName("/etc/os-release")
+
+    let kernelName = 
+        getKernel("/proc/version")
+
+    let (shell:string) = 
+        getShell("SHELL")
+
+    let user = 
+        getUser("USER")
+
+    let hostName = 
+        getHostName("/etc/hostname")
+
+    let memInfo = 
+        getMemoryInfo("/proc/meminfo")
+        
+    let localIp = 
+        getLocalIpAddress() 
+
+    let publicIp = 
+        getPublicIpAddressAsync("http://ident.me") |> Async.RunSynchronously
+    
+    let uptime = 
+        getUptime("/proc/uptime")
+
+    let (rows:IRenderable seq) = 
+        seq {
+            new Text($"Distribution: {distroName}", new Style(Color.DarkRed)) :> IRenderable
+            new Text($"Kernel: {distroName}", new Style(Color.DarkRed)) :> IRenderable
+            new Text($"Shell: {shell}", new Style(Color.DarkRed)) :> IRenderable
+            new Text($"User: {user}", new Style(Color.Yellow)) :> IRenderable
+            new Text($"Hostname: {hostName}", new Style(Color.Yellow)) :> IRenderable
+            new Text($"Uptime: {uptime}", new Style(Color.Blue)) :> IRenderable
+            new Text($"Memory: {memInfo}", new Style(Color.Blue)) :> IRenderable
+            new Text($"LocalIP: {localIp}", new Style(Color.Green)) :> IRenderable
+            new Text($"PublicIP: {publicIp}", new Style(Color.Green)) :> IRenderable
+        }
+
+ 
+    let distroIdFig = new FigletText(distroId)
+    distroIdFig.Color <- Color.Green
+
+    AnsiConsole.Write(distroIdFig)
+    AnsiConsole.Write(new Rows(rows))
+
+DisplayInfo()
---
+++ README.md
@@ -0,0 +1,51 @@
+# Fitch
+
+Command line system information display utility for Linux systems built with .NET.
+
+![Fitch CLI Tool](./images/fitch-display.png)
+
+**NOTE: This application should work on most Linux systems. However, it's only been tested on Arch-based distributions like Manjaro.**
+
+## Dependencies
+
+- [Spectre.Console](https://spectreconsole.net/)
+
+## Build from source
+
+### Prerequisites
+
+- [.NET 7 SDK](https://dotnet.microsoft.com/download/dotnet/7.0) 
+
+### Instructions
+
+1. Clone [fitch repo]()
+1. Nagivate to the project directory.
+1. Run the build command using dotnet CLI
+
+    ```dotnetcli
+    dotnet build
+    ```
+
+1. Run the publish command using the dotnet CLI
+
+    ```dotnetcli
+    dotnet publish -c Release
+    ```
+
+    Running this command will generate an executable in the `
+
+
+## Run application
+
+1. Copy the executable from the publish directory (`bin/Release/net7.0/linux-x64/publish`) to the `/usr/bin/` directory.
+1. Run the following command in your terminal
+
+    ```bash
+    fitch
+    ```
+
+1. (Optional) Add the `fitch` command to your shell config file to start when your shell starts. 
+
+## Acknowledgements
+
+This project was inspired by [Nitch](https://github.com/unxsh/nitch) and [Neofetch](https://github.com/dylanaraps/neofetch)
